4695ca8911fd7dad477e800f0a86b674191c0c05
sidash <sidash.seo@gmail.com>
2018-02-22 14:27:00 +0200

    Small changes 22.02.2018

--- wp-content/themes/miskspb/work.php
+++ wp-content/themes/miskspb/work.php
@@ -14,63 +14,63 @@
 				<div class="work_end">
 					<div class="object object_one">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства жилого комплекса в квартале 31 южнее реки волковки (юрв, квартал 31) в увязке с проектом реконструкции квартала 18 иосточнее витебской железной дороги (ввжд, квартал 18)</p>
 						<p class="bot_for_slide">Санкт-Петербург, Фрунзенский район</p>
 					</div>
 					<div class="object object_two">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства жилого комплекса «Ладожский парк»</p>
 						<p class="bot_for_slide">Санкт-Петербург, Невский район, западная часть квартала 23-23А севернее ул. Новоселов (СУН, квартал 23-23А)</p>
 					</div>
 					<div class="object object_three">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства жилого дома со встроенными помещениями</p>
 						<p class="bot_for_slide">Санкт-Петербург, СУН, квартал 8А, корпус 25</p>
 					</div>
 					<div class="object object_four">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства туристического информационно-делового центра</p>
 						<p class="bot_for_slide">г. Петергоф, Торговая площадь, участок 1</p>
 					</div>
 					<div class="object object_five">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства многофункционального комплекса зданий жилого и торгового назначения</p>
 						<p class="bot_for_slide">Санкт-Петербург, Фрунзенский район, восточнее Витебской железной дороги, квартал 12А</p>
 					</div>
 					<div class="object object_six">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства жилого комплекса</p>
 						<p class="bot_for_slide">Санкт-Петербург, Приморский район, МО Озеро Долгое, квартал 23Б</p>
 					</div>
 					<div class="object object_seven">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Инвестиционный проект строительства жилого комплекса</p>
 						<p class="bot_for_slide">Санкт-Петербург, Приморский район, Каменка, квартал 75А, 78А</p>
 					</div>
 					<div class="object object_eight">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Проектирование и реконструкция здания под гостиницу</p>
 						<p class="bot_for_slide">Санкт-Петербург, Конногвардейский бульвар, дом 5. «МОНФЕРРАН»</p>
 					</div>
 					<div class="object object_nine">
 						<img src="/wp-content/themes/miskspb/img/zaversionnie.png" alt="MISK_zaversionnie">
-						<a href="/zavershennyie-obektyi/"><h3>Завершенные проекты</h3></a>
+						<a href="/zavershennyie-obektyi/"><h3>Завершенные объекты</h3></a>
 						<hr>
 						<p>Проектирование и строительство жилого комплекса со встроенно-пристроенными помещениями и подземными автостоянками</p>
 						<p class="bot_for_slide">Санкт-Петербург, Красногвардейский район, Свердловская наб., дом 54-58, ЖК PLATINUM.</p>
